f7730efdd3d329f924b699dbc82c647a7a5cbc62
Sosnovschenko Roman <memphis@com.mels.ru>
2018-11-19 20:57:04 +0300

    Flask demo application

---
+++ camera.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy
+
+
+class VideoCamera(object):
+    class_var = 0
+    video = 0
+    url = 'http://127.0.0.1:8081/video.mjpg'
+    count = 0
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        if VideoCamera.class_var == 0:
+            VideoCamera.video = cv2.VideoCapture(0)
+            #VideoCamera.video = cv2.VideoCapture(self.url)
+        VideoCamera.class_var += 1
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        # self.video = cv2.VideoCapture('video.mp4')
+
+    def __del__(self):
+        VideoCamera.class_var -= 1
+        if VideoCamera.class_var == 0:
+            VideoCamera.video.release()
+
+    def get_frame(self):
+        success, image = VideoCamera.video.read()
+
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
---
+++ start2.py
@@ -0,0 +1,135 @@
+import argparse
+import json
+import time
+
+import imutils as imutils
+import numpy as np
+import cv2
+
+from flask import Flask, render_template, Response
+from camera2 import VideoCamera
+
+app = Flask(__name__)
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+    help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+    help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+    help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+    "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+    "sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_jpeg()
+        time.sleep(1.0)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen2(camera):
+    while True:
+        frame = camera.get_frame()
+        frame = imutils.resize(frame, width=400)
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+                                     0.007843, (300, 300), 127.5)
+
+        # pass the blob through the network and obtain the detections and
+        # predictions
+        net.setInput(blob)
+        detections = net.forward()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def gen3(camera):
+    frame = camera.get_frame()
+    frame = imutils.resize(frame, width=400)
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
+
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    net.setInput(blob)
+    detections = net.forward()
+    obj_count = 0
+
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence > args["confidence"]:
+            # extract the index of the class label from the
+            # `detections`, then compute the (x, y)-coordinates of
+            # the bounding box for the object
+            idx = int(detections[0, 0, i, 1])
+            if idx != 15:
+                continue
+            obj_count += 1
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # draw the prediction on the frame
+            label = "{}: {:.2f}%".format(CLASSES[idx],
+                                         confidence * 100)
+            cv2.rectangle(frame, (startX, startY), (endX, endY),
+                          COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(frame, label, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+    info = "pers: " + str(obj_count);
+    #return json.dumps({"api": '1.0'})
+    return json.dumps({"api": info})
+
+def success_handle(output, status=200, mimetype='application/json'):
+    return Response(output, status=status, mimetype=mimetype)
+
+
+@app.route('/video_feed2')
+def video_feed2():
+    output = gen3(VideoCamera())
+    return success_handle(output)
+
+
+@app.route('/api', methods=['GET'])
+def homepage():
+    output = json.dumps({"api": '1.0'})
+    return success_handle(output)
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', debug=True, threaded=True)
